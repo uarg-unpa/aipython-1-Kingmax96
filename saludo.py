dc35fc1d3ddfaf7baa22f2d8960978fe429a4d2f
Kingmax96 <99357334+Kingmax96@users.noreply.github.com>
2023-10-09 19:50:31 +0000

    comenzaro elrepo1

---
+++ saludo.py
@@ -0,0 +1,8 @@
+def saludo(nom, ap)
+
+    nombre, apellido = nom , ap
+    print (f"hola perras, soy {nombre},puedo darles el mundo si quiere, recuerde que soy {apellido}")
+
+
+saludo (maxi, kingma)
+print (saludo)
